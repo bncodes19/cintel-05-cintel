55da38fe6d61fdb81bade3f4f0e7ce871d7eb20b
Brett Neely <154631645+bncodes19@users.noreply.github.com>
2024-04-07 11:34:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ import pandas as pd
 import plotly.express as px
 from shinywidgets import render_plotly
 from scipy import stats
-# https://fontawesome.com/v4/cheatsheet/
 from faicons import icon_svg
 
 # Constant update interval for live data set to every 3 seconds
 UPDATE_INTERVAL_SECS: int = 3
 
 # Initialize reactive value with common data structure
-DEQUE_SIZE: int = 5
+DEQUE_SIZE: int = 10
 reactive_value_wrapper = reactive.value(deque(maxlen=DEQUE_SIZE))
 
 
@@ -45,8 +44,6 @@ def reactive_calc_combined():
     # Every time we call this function, we'll get all these values
     return deque_snapshot, df, latest_dictionary_entry
 
-
-
 # Shiny UI Page layout
 ui.page_opts(title="Live Temperature Dashboard", fillable=True)
 
@@ -67,7 +64,7 @@ with ui.sidebar(position="right", bg="#f8f8f8", open="open"):
 with ui.layout_columns():
     with ui.value_box(
         showcase=icon_svg("sun"),
-        theme="bg-gradient-blue-purple",
+        theme="bg-gradient-red-cyan",
     ):
         "Current Temperature"
         @render.text
@@ -76,29 +73,26 @@ with ui.layout_columns():
             deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
             return f"{latest_dictionary_entry['temp']} F"
         "warmer than usual"
-
-  
-
-    with ui.card(full_screen=True):
-        ui.card_header("Current Date and Time")
-
+    with ui.value_box(
+        showcase=icon_svg("clock"),
+        theme="bg-gradient-purple-cyan",
+    ):
+        "Current Date and Time"
         @render.text
-        def display_time():
-            """Get the latest reading and return a timestamp string"""
+        def display_current_date_time():
+            """Get the latest reading and return a temperature string"""
             deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
-            return f"{latest_dictionary_entry['timestamp']}"
-
-
-#with ui.card(full_screen=True, min_height="40%"):
-with ui.card(full_screen=True):
-    ui.card_header("Most Recent Readings")
-
-    @render.data_frame
-    def display_df():
-        """Get the latest reading and return a dataframe with current readings"""
-        deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
-        pd.set_option('display.width', None)        # Use maximum width
-        return render.DataGrid( df,width="100%")
+            return f"{latest_dictionary_entry['timestamp']}"    
+  
+with ui.layout_columns():
+    with ui.card(full_screen=True):
+        ui.card_header("Most Recent Readings")
+        @render.data_frame
+        def display_df():
+            """Get the latest reading and return a dataframe with current readings"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            pd.set_option('display.width', None)
+            return render.DataGrid( df,width="100%")
 
 with ui.card():
     ui.card_header("Chart with Current Trend")
@@ -112,23 +106,14 @@ with ui.card():
         if not df.empty:
             # Convert the 'timestamp' column to datetime for better plotting
             df["timestamp"] = pd.to_datetime(df["timestamp"])
-
-            # Create scatter plot for readings
-            # pass in the df, the name of the x column, the name of the y column,
-            # and more
-        
+ 
             fig = px.scatter(df,
             x="timestamp",
             y="temp",
             title="Temperature Readings with Regression Line",
-            labels={"temp": "Temperature (°C)", "timestamp": "Time"},
+            labels={"temp": "Temperature (°F)", "timestamp": "Time"},
             color_discrete_sequence=["blue"] )
             
-            # Linear regression - we need to get a list of the
-            # Independent variable x values (time) and the
-            # Dependent variable y values (temp)
-            # then, it's pretty easy using scipy.stats.linregress()
-
             # For x let's generate a sequence of integers from 0 to len(df)
             sequence = range(len(df))
             x_vals = list(sequence)
